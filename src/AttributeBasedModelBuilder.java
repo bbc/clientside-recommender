141c59064c2d44f43a46a2c2eade752274b779f4
Chris Newell <chris.newell@rd.bbc.co.uk>
2013-05-29 14:19:29 +0100

    Added example training app for collaborative filtering

--- src/AttributeBasedModelBuilder.java
+++ src/AttributeBasedModelBuilder.java
@@ -1,5 +1,3 @@
-package bbc.rd.mymedialite.train;
-
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -14,53 +12,38 @@ import org.mymedialite.datatype.SparseBooleanMatrix;
 
 /**
  * Utility for building for item attribute based kNN models 
+ * See TODO below for where additional data is required 
  * @author Chris Newell, BBC R&D
  */
 public class AttributeBasedModelBuilder {
 
   // Configuration
-  protected static final String LOG_FILE = "ModelBuilderExample.log";
-  protected static final String MODEL_FILE_PATH = "receng-sp-model.js";
-  private static final int K = 20;
-  private static final String timestamp = "2013-04-23T10:00:00Z";  // TODO Set as required
+  public static final String MODEL_FILE_NAME = "receng-model.js";
+  public static final int K = 20;
+  protected static EntityMapping attributeMapping = new EntityMapping();
   
-  public static void main(String[] args) throws IOException {
-    createModel(MODEL_FILE_PATH);
+  public static void main(String[] args) {
+    try {
+      createModel(MODEL_FILE_NAME);
+    } catch (Throwable e) {
+      e.printStackTrace();
+    }
   }
 //--------------------------------------------------------------------------------------------------
   public static void createModel(String filename) throws IOException {
-   
-    EntityMapping itemMapping = new EntityMapping();
-    EntityMapping attributeMapping = new EntityMapping();
-
-    List<String> orderedItems = new ArrayList<String>();  // TODO add your item identifiers to this list in the required default order (we use item popularity)
-
-    // Write metadata
-    PrintWriter writer = new PrintWriter(new FileWriter(filename));
-    writer.println("Receng.timestamp = \"" + timestamp + "\"");
-    writer.print("Receng.items = new Array(");
-    boolean firstItem = true;
     
-    SparseBooleanMatrix itemAttributes = new SparseBooleanMatrix();
-    for(String item : orderedItems) {
-      int itemId = itemMapping.toInternalID(item);
+    String timestamp = "2013-04-23T10:00:00Z";  // TODO Set as required
+    
+    List<String> orderedItemIds = new ArrayList<String>();  // TODO add your item identifiers to this list in the required default order (we use item popularity)
 
+    SparseBooleanMatrix itemAttributes = new SparseBooleanMatrix();
+    for(int i=0; i < orderedItemIds.size(); i++) {
       Set<String> attributes = new HashSet<String>();  // TODO add the attributes for the item to this list
       for(String attribute : attributes) {
         Integer attributeId = attributeMapping.toInternalID(attribute);
-        itemAttributes.set(itemId, attributeId, true);
+        itemAttributes.set(i, attributeId, true);
       }
-      
-      // TODO Set the following metadata fields for each item
-      String title = "Item title";
-      int genre = 0;      // Use a lookup table e.g. Comedy: 0, Drama: 1, Factual: 3, etc
-      int mediaType = 0;  // e.g. Video: 1, Audio: 0 
- 
-      if(firstItem) { firstItem = false; } else { writer.print(","); }
-      writer.print("{p:\"" + item + "\",t:\"" + title + "\",g:" + genre + ",v:" + mediaType + "}");       
     }
-    writer.println(");");
-    writer.println();
 
     // Build the correlation matrix
     CorrelationMatrix correlationMatrix = BinaryCosine.create(itemAttributes);
@@ -68,6 +51,26 @@ public class AttributeBasedModelBuilder {
     int[][] nn = new int[numItems][];
     for (int i = 0; i < numItems; i++)  nn[i] = correlationMatrix.getNearestNeighbors(i, K);
 
+    // Write metadata
+    PrintWriter writer = new PrintWriter(new FileWriter(filename));
+    writer.println("Receng.timestamp = \"" + timestamp + "\"");
+    writer.print("Receng.items = new Array(");
+    boolean firstItem = true;
+    
+    for(int i=0; i < orderedItemIds.size(); i++) {
+      String itemId = orderedItemIds.get(i);
+      
+      // TODO Set the following metadata fields for each item
+      String title = "Item title";
+      int genre = 0;                // Use a lookup table e.g. Comedy: 0, Drama: 1, Factual: 3, etc
+      int mediaType = 0;            // e.g. Video: 1, Audio: 0 
+ 
+      if(firstItem) { firstItem = false; } else { writer.print(","); }
+      writer.print("{p:\"" + itemId + "\",t:\"" + title + "\",g:" + genre + ",v:" + mediaType + "}");       
+    }
+    writer.println(");");
+    writer.println();
+    
     // Write the nearest neighbor ids
     writer.print("Receng.nn = new Array(");
     boolean firstRow = true;
